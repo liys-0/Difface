8ec03939a808d2d04421bfeb460bc197a68855e0
liys-0 <liys.85@gmail.com>
2026-05-05 13:53:41 +0800

    add positional encoder

--- Difface/faceclip/encoder.py
+++ Difface/faceclip/encoder.py
@@ -101,20 +101,21 @@ class Transformer(nn.Module):
     def __init__(self, num_snps=7842):
         super(Transformer, self).__init__()
         self.fc1 = nn.Linear(num_snps, 1024)
-        self.embedding_layer = nn.Embedding(1,4)
         self.relu1 = nn.ReLU()
         self.relu2 = nn.ReLU()
+        self.pos_enc = nn.Parameter(torch.randn(1, 1024, 64))
         self.layer1 = nn.TransformerEncoderLayer(d_model=64, nhead=8, dim_feedforward=512, dropout=0.1, layer_norm_eps=1e-05)
         self.layer2 = nn.TransformerEncoderLayer(d_model=64, nhead=8, dim_feedforward=512, dropout=0.1, layer_norm_eps=1e-05)
 
         self.fc2 = nn.Linear(64*1024, 128)
-        
+
     def forward(self, x):
         x = x.float()
         x = self.relu1(self.fc1(x))
-        x = x.unsqueeze(2)  
-        x = x.expand(-1, -1, 64)  
-        x = x.permute(1, 0, 2) 
+        x = x.unsqueeze(2)
+        x = x.expand(-1, -1, 64)
+        x = x + self.pos_enc
+        x = x.permute(1, 0, 2)
         x1 = self.layer1(x) 
         x1 = x1 + x
         x2 = self.layer2(x1)
